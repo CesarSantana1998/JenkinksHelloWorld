3a3812a6ac1cbde19c34c5124475910b9ad5741f
CesarSantana1998 <117761182+CesarSantana1998@users.noreply.github.com>
2022-11-10 11:59:21 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Que onda banda");
+			System.out.println("Made some changes");
 	}
 
 }
